55fee2f960641e22edf2bbaba823ae19ef845021
Anderson Coelho <anderson.coelho1969@gmail.com>
2021-01-23 13:38:41 -0400

    Criando o formulario de cadastro

--- pages/cadastrar.php
+++ pages/cadastrar.php
@@ -16,5 +16,47 @@
                 </ul>
             </div>
         </header>
+        <div>
+            <div>
+                <form method="POST" action="">
+                    <label>
+                        Nome do projeto:<br>
+                        <input type="text" name="nameProj">
+                    </label><br><br>
+
+                    <label>
+                        Data de inicio:<br>
+                        <input type="text" name="startDate">
+                    </label><br><br>
+
+                    <label>
+                        Data de término:<br>
+                        <input type="text" name="endDate">
+                    </label><br><br>
+
+                    <label>
+                        Valor do projeto:<br>
+                        <input type="text" name="valueProj">
+                    </label><br><br>
+
+                    <label>
+                        Risco:<br>
+                        <select name="risk">
+                            <option value=""></option>
+                            <option value="0">Pequeno</option>
+                            <option value="1">Mediano</option>
+                            <option value="2">Grande</option>
+                        </select>
+                    </label><br><br>
+
+                    <label>
+                        Membros do projeto:<br>
+                        <textarea name="contributors" cols="30" rows="5"></textarea>
+                    </label><br><br>
+
+                    <input type="submit" value="Cadastrar">
+                </form>
+            </div>
+        </div>
     </body>
 </html>
