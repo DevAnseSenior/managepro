69ddd8a7e493d0e8e505a194bb2baf1dd2207a74
Anderson Coelho <anderson.coelho1969@gmail.com>
2021-01-24 13:26:50 -0400

    Estilizando a pagina principal do sistema

--- index.php
+++ index.php
@@ -5,29 +5,33 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Home - ManagePRO</title>
         <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/default.css">
+        <link rel="stylesheet" href="assets/css/main.css">
     </head>
     <body>
-        <header>
-            <div>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="pages/projetos.php">Projetos</a></li>
-                    <li><a href="pages/sobre.php">Sobre</a></li>
-                </ul>
-            </div>
-        </header>
-        <div>
-            <div>
-                Alguma coisa
-            </div>
-        </div>
-        <footer>
-            <div>
-                <div>
-                    Todos os direitos reservados©Lorem Ipsum Inc.
+        <div class="container">
+            <header class="container">
+                <div class="logo"><a href="">ManagePRO</a></div>
+                <nav>
+                    <ul class="nav">
+                        <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="pages/projetos.php">Projetos</a></li>
+                        <li class="nav-item"><a class="nav-link" href="pages/sobre.php">Sobre</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <div class="jumbotron jumbotron-fluid front">
+                <div class="container">
+                    <h1 class="display-4">ManagePRO</h1>
+                    <p class="lead">é um sistema que visa gerir os sistemas desenvolvidos por empresas de forma que essa gerência seja mais eficaz possível. Onde além de visualizar os principais pontos o cliente possa tambem gerênciar riscos e possíveis retornos que o projeto possa ter. </p>
                 </div>
             </div>
-        </footer>
+            <footer>
+                <div class="copyright">
+                    Todos os direitos reservados©Lorem Ipsum Inc.
+                </div>
+            </footer>
+        </div>
 
         <script src="assets/js/jquery-3.5.1.min.js"></script>
         <script src="assets/js/bootstrap.bundle.min.js"></script>
